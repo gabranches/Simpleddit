e44b183d9e1017592a62183b2e98a72f0978bec2
gvdasolutions <gabranches@gmail.com>
2015-01-26 23:28:53 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,177 @@
+<?php
+
+$sub = "";
+
+if(isset($_GET["r"])){
+	$sub = $_GET["r"];
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<title>simplereddit</title>
+<link id="favicon" rel="shortcut icon" type="image/png" href="" />
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="style.css">
+
+<script src="lib/handlebars-v2.0.0.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="https://d3dy5gmtp8yhk7.cloudfront.net/2.1/pusher.min.js"></script>
+
+</head>
+
+<body>
+
+<nav id="topnav"class="row-fluid navbar navbar-fixed-top" role="navigation">
+	<div class="container-fixed">
+		<div id="formrow" class="row">
+			<form id="form-sub" role="form">
+				<div id="logo" class="col-xs-1"><a href="/">simplereddit</a></div>
+				<div class="col-xs-2">
+					<select id="select-sub" placeholder="Select Subreddit" class="form-control">
+						<option selected="selected" disabled>Popular Subreddits</option>
+						<option value="">Front Page</option>
+					</select>
+				</div>
+				<div class="col-xs-2">
+					<input type="input" class="form-control" id="input-sub" placeholder="Enter Subreddit">
+				</div>
+				<div class="col-xs-1">
+					<select id="select-sort" class="form-control">
+					    <option selected="selected" value="hot">hot</option>
+					    <option value="new">new</option>
+					    <option value="rising">rising</option>
+					    <option value="day">top today</option>
+					    <option value="week">top this week</option>
+					    <option value="month">top this month</option>
+					    <option value="year">top this year</option>]
+					    <option value="all">top all time</option>
+					</select>
+				</div>
+				<div class="col-xs-1">
+					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+				</div>
+				<div class="col-xs-3"></div>
+				<div class="col-xs-2"></div>
+				
+				
+			</form>
+		</div>
+		<div class="row" id="main-top">
+			<div class="col-xs-6">
+				<div class="col-xs-3 col-md-2 col-lg-1 text-center text-center"><span class="glyphicon glyphicon-arrow-up"></span></div>
+				<div id="subnameheader" class="col-xs-6 col-md-8 col-lg-10 text-center text-center"></div>
+				<div class="col-xs-3 col-md-2 col-lg-1 text-center text-center"><span class="glyphicon glyphicon-comment"></span></div>
+			</div>
+			<div class="col-xs-6">
+				<div id="storyheader" class="col-xs-12 text-center"></div>
+			</div>
+		</div>
+	</div>
+</nav>
+
+<div class="container-fixed">
+	<div id="leftcolumn" class="col-xs-6">
+		<div class="row">
+			<div id="main" class="col-xs-12"></div>
+		</div>
+		<div class="row">
+			<div id="getmore" class="col-xs-12 text-center">Load more...</div>
+		</div>
+		
+	</div>
+	<div id="rightcolumn" class="col-xs-6">
+		<div id="options" style="display:none" class="row">
+			<div class="col-xs-6">
+				Change page title: <br /><br />
+				<input type="input" class="form-control" id="pagetitleinput" placeholder="rViewer">
+			</div>
+		</div>
+		<div class="row">
+			<div id="story" class="col-xs-12"></div>
+		</div>
+		<div class="row">
+			<div id="comments" class="col-xs-12"></div>
+		</div>
+	</div>
+</div>
+
+</body>
+
+</html>
+
+<script id="entry-template" type="text/x-handlebars-template">
+
+	<div data-id="{{id}}" data-sub="{{subreddit}}" class="row entries">
+		<div class="col-xs-3 col-md-2 col-lg-1 text-center">{{score}}</span></div>
+<div class="col-xs-6 col-md-8 col-lg-10 title">{{{htmlHelper title}}} <small><span class="red">{{nsfwHelper over_18}} </span>{{subHelper this}}</small> </div>
+	<div class="col-xs-3 col-md-2 col-lg-1 text-center">{{num_comments}}<br /></span></div>
+	
+</div>
+
+</script>
+
+<script id="story-template" type="text/x-handlebars-template">
+	<div class="row">
+		<div class='story-title col-xs-12'>
+			<strong>{{{htmlHelper title}}}</strong><br /><small>{{author}} - {{timeHelper created_utc}}</small>
+ 	</div>
+</div>
+<div class="row">
+		<div class='story-selftext col-xs-12'>
+			{{{htmlHelper selftext_html}}}
+			{{{picHelper this}}}
+ 	</div>
+</div>
+<div class="row">
+ 	<div class='story-url col-xs-12'>
+ 		<a href="{{url}}" target="_blank">{{url}}</a>
+ 	</div>
+ </div>
+ <div class="row">
+ 	<div class='story-sublink col-xs-12'>
+ 		<strong><span>{{subreddit}}</span></strong>
+ 	</div>
+ </div>
+ 
+</div>
+
+</script>
+
+<script id="comment-template" type="text/x-handlebars-template">
+ 	<div style="{{indentHelper this}} {{colorHelper numNest}}" class='comment col-xs-12 '>
+ 		<div class="row">
+ 			<div class="col-xs-12">
+ 				<small>{{ptsHelper ups}}</small>  <small>{{author}}</small>   <small>{{timeHelper created_utc}}</small>
+ 			</div>
+ 		</div>
+ 		<div class="row">
+ 			<div class="col-xs-12">
+	 			{{{htmlHelper body_html}}}
+	 		</div>
+	 	</div>
+ 	</div>
+</div>
+
+</script>
+
+<script>
+
+	sub = ""; // For local version
+	sub = <?php echo json_encode($sub); ?>;
+
+</script>
+
+<script src="main.js"></script>
+<script src="handlebars_helpers.js"></script>
---
+++ main.js
@@ -0,0 +1,281 @@
+$(function()
+{
+	loading = "Loading <img id='loadgif' src='images/ajax-loader.gif' />";
+	getPopularSubs();
+	maxNest = 15;
+	resize();
+	after = null;
+	count = 0;
+	limit = 47;
+	sort = "hot";
+	getItems(sub, sort);
+	
+	
+	// Build Handlebars templates
+
+	var raw_template = $('#entry-template').html();
+	entryTemplate = Handlebars.compile(raw_template);
+	entryPlaceHolder = $("#main");
+
+	var raw_template = $('#comment-template').html();
+	commentTemplate = Handlebars.compile(raw_template);
+	commentPlaceHolder = $("#comments");
+
+	var raw_template = $('#story-template').html();
+	storyTemplate = Handlebars.compile(raw_template);
+	storyPlaceHolder = $("#story");
+
+}); 
+
+// ** EVENT HANDLERS ** //
+
+$("#form-sub").submit(function(e) // Subreddit form submit
+{ 	
+	e.preventDefault();
+	sub = $("#input-sub").val();
+	ClearMain();
+	getItems(sub, sort);
+});
+
+$(document).on("click", ".story-sublink span", function() // Go to subreddit on click
+{ 	
+	sub = $(this).html();
+	ClearMain();
+	$("#input-sub").val(sub);
+	getItems(sub, sort);
+});
+
+$(document).on("click", ".entries", function() // Go to story
+{ 	
+	ClearComments();
+	id = $(this).attr("data-id");
+	getStory($(this).attr("data-sub"),id);
+});
+
+$(document).on("click", "#showimage", function() // Show story image
+{ 	
+	$('#storyimage').toggle();
+	$('#showimage').text() == " Show Image" ? $('#showimage').html("<span class='glyphicon glyphicon-picture'></span> Hide Image") : $('#showimage').html("<span class='glyphicon glyphicon-picture'></span> Show Image");
+});
+
+$("#getmore").click(function() // Load more
+{
+	getItems(sub, sort);
+});
+
+$("#select-sort").change(function() // Sort dropdown submit
+{ 	
+	sort = $("#select-sort").find(":selected").val();
+	ClearMain();
+	getItems(sub, sort);
+});
+
+$("#select-sub").change(function() // Dropdown submit
+{ 	
+	sub = $("#select-sub").val();
+	ClearMain();
+	$("#input-sub").val(sub);
+	getItems(sub, sort);
+});
+
+$(window).resize(function(){
+	resize();
+});
+
+// ** FUNCTIONS ** //
+
+function resize()	// Resize containers when window changes
+{
+	var ht = $(window).height();
+	$("#leftcolumn").css("height", ht-91 + "px");
+	$("#rightcolumn").css("height", ht-91 + "px");
+}
+
+function ClearMain() // Clear all stories
+{
+	$("#getmore").hide();
+	$("#main").html("");
+	count = 0;
+	after = "";
+}
+
+function ClearComments() // Clear all stories
+{
+	$("#story").html("");
+	$("#comments").html("");
+}
+
+function getItems(sub, sort) // Get stories
+{
+	var subUrl 		= (sub == "" ) ? "" : "r/"+sub;
+	var limitUrl 	= "limit=" + limit;
+	var afterUrl 	= (after == null) ? "" : "&after="+after;
+	var countUrl 	= (count == 0) ? "" : "&count="+count;
+
+	$("#subnameheader").html(loading);
+
+	switch(sort) 
+	{	
+		case "hot":
+			var sortType = "hot";
+			var sortUrl = "sort=hot";
+			break;
+		case "new":
+			var sortType = "new";
+			var sortUrl = "sort=new";
+			break;
+		case "rising":
+			var sortType = "rising";
+			var sortUrl = "sort=rising";
+			break;
+		case "day":
+			var sortType = "top";
+			var sortUrl = "sort=top&t=day";
+			break;
+		case "week":
+			var sortType = "top";
+			var sortUrl = "sort=top&t=week";
+			break;
+		case "month":
+			var sortType = "top";
+			var sortUrl = "sort=top&t=month";
+			break;
+		case "year":
+			var sortType = "top";
+			var sortUrl = "sort=top&t=year";
+			break;
+		case "all":
+			var sortType = "top";
+			var sortUrl = "sort=top&t=all";
+			break;
+	}
+	
+	$("#subnameheader").html(loading);
+	$("#getmore").html(loading);
+	
+	var url = "http://www.reddit.com/" + subUrl + "/" + sortType + "/.json?" + sortUrl + "&" + limitUrl + afterUrl + countUrl;
+
+	$.getJSON( url, function(data) 
+	{
+		listItems(data, sub);
+		$("#getmore").show();
+		$("#getmore").html("Load more...");
+		if(sub=="") // Change sub name header
+		{
+		  $("#subnameheader").html("<a target='_blank' href='http://www.reddit.com/'>Front Page</a>");
+		} 
+		else 
+		{
+		  $("#subnameheader").html("<a target='_blank' href='http://www.reddit.com/r/" + sub + "'>r/"+sub+"</a>");
+		}
+	}
+	).fail(function(data) 
+	{
+		ClearMain();
+		$("#subnameheader").html("<div class='col-xs-12'>Could not get data from subreddit '"+sub+"'. Please make sure that this subreddit exists, or try again in a few minutes.</div>");
+	});
+
+}
+
+function getPopularSubs()
+{
+	$.getJSON("http://www.reddit.com/subreddits/popular/.json?limit=100", function(data)
+	{
+		$.each(data.data.children,function(index,element)
+		{ 
+			$("#select-sub").append("<option value='"+element.data.display_name+"' label='"+element.data.display_name+"'></option>");
+		});
+	})
+}
+
+function listItems(data,sub)
+{
+	$.each(data.data.children,function(index,element)
+	{ 
+		element.data.topsub = sub;
+		var html = entryTemplate(element.data);
+		entryPlaceHolder.append(html);
+		count++;
+		after = element.data.name;
+	});
+}
+
+function getStory(sub,id)
+{
+	var url = "r/"+sub+"/comments/"+id;
+
+	$("#storyheader").html(loading);
+
+	var requestUrl = "http://www.reddit.com/"+url+"/.json";
+
+	$.getJSON(requestUrl, function(data)
+	{
+		$("#storyheader").html("<a target='_blank' href='http://www.reddit.com/"+url+"'>"+url+"</a>");
+
+		$.each(data,function(index,element)
+		{ 
+			$.each(element.data.children, function(index, element)
+			{
+
+				if(element.data.title){
+					printTitle(element);
+				}
+				else
+				{
+					printComment(element,-15, 15, 0);
+				}
+			});
+		});
+		$("a[href^='http://']").attr("target","_blank");
+		$("a[href^='https://']").attr("target","_blank");
+	})
+}
+
+function printTitle(data)
+{
+	var html = storyTemplate(data.data);
+	storyPlaceHolder.append(html); 
+}
+
+function printComment(data,indent1,indent2,numNest) // Recursive function to print comments
+{
+	data = data.data;
+
+	if(data.body)
+	{
+		data.indent1 = indent1;
+		data.indent2 = indent2;
+		data.numNest = numNest;
+		var html = commentTemplate(data);
+		commentPlaceHolder.append(html); 
+	}
+
+	if(data.replies)
+	{
+		$.each(data.replies.data.children, function(index, element)
+		{
+			if(numNest<maxNest)
+			{
+				printComment(element, (indent1+25), (indent2+25), numNest+1);
+			}
+		});
+	} 
+}
+
+function htmlDecode(input)  // Unescape html
+{
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+function getYoutubeId(url)  // Returns youtube data-id
+{
+	var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+	if(videoid != null) {
+	   return videoid;
+	} else { 
+	    console.log("The youtube url is not valid.");
+	}
+}
+
